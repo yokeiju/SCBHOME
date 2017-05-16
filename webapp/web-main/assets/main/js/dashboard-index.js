1148ebc741e56162ceccca58db29b96125cb486b
yokeiju <yokeiju@gmail.com>
2017-05-16 13:49:52 +0700

    with dummy data

--- webapp/web-main/assets/main/js/dashboard-index.js
+++ webapp/web-main/assets/main/js/dashboard-index.js
@@ -9,9 +9,87 @@ dashboard.showrow = function(){
 dashboard.showlist = function(){
 	$("#panel1").hide();
 	$("#panel2").show();	
-}
+}   
+
+dashboard.data = [
+	{
+		"TitleProject":"Project 1",
+		"SubProject":"Exellerator",
+		"LinkUrl":"#",
+		"ColorSubProject":"#00b0f0"
+	},
+	{
+		"TitleProject":"Project 1",
+		"SubProject":"UAT",
+		"LinkUrl":"#",
+		"ColorSubProject":"#23c6c8"
+	},
+	{
+		"TitleProject":"Project 1",
+		"SubProject":"EACIIT",
+		"LinkUrl":"#",
+		"ColorSubProject":"#ff9966"
+	},
+	{
+		"TitleProject":"Project 1",
+		"SubProject":"Development",
+		"LinkUrl":"#",
+		"ColorSubProject":"#666699"
+	},
+		{
+		"TitleProject":"Project 2",
+		"SubProject":"Exellerator",
+		"LinkUrl":"#",
+		"ColorSubProject":"#00b0f0"
+	},
+	{
+		"TitleProject":"Project 2",
+		"SubProject":"EACIIT",
+		"LinkUrl":"#",
+		"ColorSubProject":"#ff9966"
+	},
+	{
+		"TitleProject":"Project 2",
+		"SubProject":"Development",
+		"LinkUrl":"#",
+		"ColorSubProject":"#666699"
+	},
+		{
+		"TitleProject":"Project 3",
+		"SubProject":"Exellerator",
+		"LinkUrl":"#",
+		"ColorSubProject":"#00b0f0"
+	},
+	{
+		"TitleProject":"Project 3",
+		"SubProject":"EACIIT",
+		"LinkUrl":"#",
+		"ColorSubProject":"#ff9966"
+	},
+	{
+		"TitleProject":"Project 3",
+		"SubProject":"Development",
+		"LinkUrl":"#",
+		"ColorSubProject":"#666699"
+	},
+
+]
+
+// dashboard.datalist = _.groupBy(dashboard.data, function(e) { return c.push({TitleProject:e.TitleProject}); });
+// console.log(dashboard.datalist);
+
+data = dashboard.data
+databaru = [];
+_.each(_.groupBy(data,"TitleProject"), function(v,i){
+    datatmp = []
+   _.each(_.groupBy(v,"ColorSubProject"), function(vv,ii){
+    datatmp.push(vv[0])
+ })
+ databaru.push({"data":datatmp})
+})
 
 $(function() {
 	$("#panel1").show();
 	$("#panel2").hide();
 });
+
